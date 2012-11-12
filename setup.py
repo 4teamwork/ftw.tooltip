28ee2a4ce38384140f911ee76398b5f7b2bf1c3d
Jonas Baumann <jone@jone.ch>
2012-11-12 15:06:48 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -11,28 +11,33 @@ tests_require = ['plone.app.testing',
 setup(name='ftw.tooltip',
       version=version,
       description="Apply tooltips dynamically",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
+
       keywords='',
       author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
-      url='http://plone.org/products/ftw.tooltip/',
+      url='https://github.com/4teamwork/ftw.tooltip',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', ],
       include_package_data=True,
       zip_safe=False,
+
+      install_requires=[
+        'setuptools',
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
-      install_requires=[
-          'setuptools',
-      ],
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
